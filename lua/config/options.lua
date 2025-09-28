a53ce90acd7ea4c9cff33973f4683e5c80ab6545
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2025-09-28 14:15:02 +0200

    set scrolloff option to 8

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,11 @@ vim.lsp.set_log_level("OFF")
 
 -- vim.opt.background does not work but vim.o : yes
 vim.o.background = "light"
+
+-- CAUSE AN ERROR : Unknown option ...
+-- SOLVED : Will be available in a future version (0.12)
+-- Delay in milliseconds before the autocomplete menu appears after typing (default = 0)
+--vim.o.autocompletedelay = 100
+
+-- Minimal number of screen lines to keep above and below the cursor (default = 0)
+vim.o.scrolloff = 8
