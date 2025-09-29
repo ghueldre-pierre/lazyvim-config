9735fa4c00af2c303467aa0a0d3518b19e674485
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2025-09-29 17:58:43 +0200

    disable completion.ghost_text (blink.cmp)

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -14,5 +14,16 @@
   },
 }--]]
 
--- to avoid potential warnings
-return {}
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      -- Ghost text shows a preview of the currently selected item as virtual text inline.
+      -- LazyVim by default set it to "true" via the global variable "vim.g.ai_cmp"
+      -- see :
+      -- - https://www.lazyvim.org/configuration/general#options (Default Options tab)
+      -- - https://www.lazyvim.org/extras/coding/blink#blinkcmp
+      ghost_text = { enabled = false },
+    },
+  },
+}
